9edc158f8c30a94c4b0001c090c79c63bfd3a923
ValleyFarell <valeevrustamilgamovlch1999@gmail.com>
2023-07-07 22:43:30 +0100

    Добавление кнопки меню в ответ на неправильный ввод ключа

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.filters import Text, CommandStart
 from aiogram.types import Message, CallbackQuery
+from keyboards.keyboards import create_menu_kb
 from lexicon.lexicon_RU import ANSWERS_LEXICON
 from states.states import game_states
 from database.database import users_db
@@ -8,7 +9,7 @@ other_router: Router = Router()
 
 @other_router.message(game_states.game_with_friend_state_reg)
 async def process_wrong_personal_key(message: Message):
-    await message.answer(text=ANSWERS_LEXICON['wrong_personal_key'])
+    await message.answer(text=ANSWERS_LEXICON['wrong_personal_key'], reply_markup=create_menu_kb(1))
 @other_router.message()
 async def process_other_message(message: Message):
     await message.answer(text=ANSWERS_LEXICON['other_message'])
